de176597b8138a698bfccda23bec9800d5cbf110
Quang Nguyen <quangn2595@gmail.com>
2019-07-11 11:16:18 -0700

    get section to show and hide

--- js/app.js
+++ js/app.js
@@ -13,57 +13,61 @@ function Images(url, title, description, keyword, horns) {
 
 Images.list = [];
 const optionArray = [];
-let counter =0;
 
 
 Images.prototype.displayImage = function() { 
-  // const $newImage = $('#photo-template').clone();
-
-  // $newImage.find('h2').text(this.title).attr('keyword', `${this.keyword}`);
-  // $newImage.find('p').text(this.description).attr('keyword', `${this.keyword}`);
-  // $newImage.find('img').attr({ src: this.image_url, 
-  //   alt: this.keyword});
-
-  // $('main').append($newImage);
-  // if (counter === 0) {
-  //   $('#photo-template:first-child').remove();
-  // }
-  // counter++;
-
-  console.log(typeof this.title);
   const photoTemplate = $('#photo-template').html();
   const photoTemplateScript = Handlebars.compile(photoTemplate);
   const image = {'title': this.title, 'image_url': this.image_url, 'description': this.description, 'keyword': this.keyword};
   const html = photoTemplateScript(image);
   $('main').append(html);
-
 };
 
 Images.prototype.displayOptions = function() { 
   if (!optionArray.includes(this.keyword)) {
-
     optionArray.push(this.keyword);
     const optionTemplate = $('#option-template').html();
     const optionTemplateScript = Handlebars.compile(optionTemplate);
     const option = {'keyword': this.keyword};
     const html = optionTemplateScript(option);
     $('select').append(html);
-
   }
 }
 
 function optionListener() { 
-  $('select').change( () => { 
+  $('select').change(() => { 
     const $selectedImage = $('select option:selected').text();
     if ($selectedImage === 'Filter by Keyword') {
       $('img').show();
     } else {
-      $('img').not(`[alt="${$selectedImage}"]`).hide();
-      $('h2').not(`[alt="${$selectedImage}"]`).hide();
-      $('p').not(`[alt="${$selectedImage}"]`).hide();
-      $(`img[alt="${$selectedImage}"]`).show();
-      $(`h2[keyword="${$selectedImage}"]`).show();
-      $(`p[keyword="${$selectedImage}"]`).show();
+      // $('img').not(`[alt="${$selectedImage}"]`).hide();
+      // $('h2').not(`[alt="${$selectedImage}"]`).hide();
+      // $('p').not(`[alt="${$selectedImage}"]`).hide();
+      $(`section[keyword!=${$selectedImage}]`).hide();
+      //   $(`img[alt="${$selectedImage}"]`).show();
+      //   $(`h2[keyword="${$selectedImage}"]`).show();
+      //   $(`p[keyword="${$selectedImage}"]`).show();
+      $(`section[keyword=${$selectedImage}]`).show();
     }
   })
+}
+
+function titleSortListener() { 
+  $('#title').click( () => { 
+    Images.list.sort( (a,b) => a.title > b.title)
+  })
+}
+
+function displayAllImages() { 
+  const photoTemplate = $('#photo-template').html();
+  const photoTemplateScript = Handlebars.compile(photoTemplate);
+  Images.list.forEach(object => {
+    const image = {'title': object.title, 'image_url': object.image_url, 'description': object.description, 'keyword': object.keyword};
+    const html = photoTemplateScript(image);
+    $('main').append(html);
+  })
+}
+
+function clearAllImages() { 
+
 }
